b1b28999def736f4f9fb9e6b40fe1c5db2a8125c
alwyntn <34329419+alwyntn@users.noreply.github.com>
2018-04-19 16:39:38 +0800

    update

--- Test Project/java2.js
+++ Test Project/java2.js
@@ -1,21 +1,44 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var listItem = document.querySelectorAll("li");
 
-button.addEventListener("click", function(){
-	if (input.value.length > 0) {
+function inputValue() {
+	return input.value.length;
+}
+
+function addList() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+}
+
+// function addToggle() {
+	
+// }
+
+for (var i = 0; listItem.length < 0; i++) {
+	listItem[i].addEventListener("click", function(){
+		this.classList.toggle("done");
+	})
+}
+
+
+// listItem.addEventListener("click", function(){
+// 	this.classList.toggle("done");
+// })
+
+
+button.addEventListener("click", function(){
+	if (inputValue() > 0) {
+	addList();
 	}
 })
 
 input.addEventListener("keypress", function(abc){
-	if (input.value.length > 0 && abc.which === 13) {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+	if (inputValue() > 0 && abc.which === 13) {
+	addList();
 	}
 })
+
